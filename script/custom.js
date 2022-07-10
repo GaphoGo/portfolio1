0867b48ad390e171cb2a4a42df100b3cf242d145
GaphoGo <cspattack@gmail.com>
2022-07-10 14:34:51 +0900

    1.2

--- script/custom.js
+++ script/custom.js
@@ -1,23 +1,36 @@
-$(function(){//document.ready
-  //#play에 클릭이벤트 생성
-  $("#play").click(function(){
-    $(this).css("display","none");//play버튼 제거
-    $("#stop").css("display","block");//stop버튼 표시
+$(function(){ 
+  function slideImg(){
+    var now = $(".imgs > img:first");
+    $(".imgs").stop().animate({"left":"-1170px"},1000,function(){
+      $(this).append(now).css("left","0px");
+     
+    })
+  }
+  var timer = setInterval(slideImg,3000)
 
-    $("#vid").get(0).play();//video를 play메서드로 영상재생합니다.
-  })
-  //#stop에 클릭이벤트 생성
-  $("#stop").click(function(){
-    $(this).css("display","none");
-    $("#play").css("display","block");
-    $("#vid").get(0).pause();//video를 pause메서드로 영상을 멈춰줍니다.
-    //$("#vid").get(0).currentTime = 0;//영상프레임을 처음(0)위치로 이동
-  });
+  $("#imgSlide > button").hover(//button에 hover이벤트 생성
+    function(){//mouseover시
+      clearInterval(timer); //timer에 들어있는 반복함수를 제거
+    },function(){//mouseout시
+      timer = setInterval(slideImg,3000);
+      //timer변수에 반복함수를 다시 할당합니다.
+    }
+  )
 
-  //영상이 끝난 뒤 구문 실행
-  $("#vid").on("ended",function(){
-    $("#play").css("display","block");
-    $("#stop").css("display","none");
-    $("#vid").get(0).load();//video를 다시 로드하여 초기상태로 돌림
+  //next버튼 클릭시
+  $(".next").click(function(){ //.next버튼 클릭시
+    if($(".imgs").is(":animated")) return false;
+
+    var now = $(".imgs > img:first");
+    $(".imgs").animate({"left":"-1170px"},300,function(){
+      $(this).append(now).css("left","0px");
+    })
   })
- });
+
+  //prev버튼 클릭시
+  $(".prev").click(function(){
+    if($(".imgs").is(":animated")) return false;
+    var last = $(".imgs > img:last");//마지막 이미지 요소를 last변수에 할당.
+    $(".imgs").prepend(last).css("left","-1170px").animate({"left":0},300);
+  })
+});
